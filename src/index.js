0fb80157acb0cd4ba35e98bd7519076a59c5a9e9
OksiDavydova <oksana.acva@gmail.com>
2021-08-13 16:28:38 +0300

    hw11-1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-
+import './sass/style.scss';
 const colors = [
   '#FFFFFF',
   '#2196F3',
@@ -11,3 +11,28 @@ const colors = [
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+const startBtn = document.querySelector(['[data-action="start"]']); 
+const stopBtn = document.querySelector(['[data-action="stop"]']);
+const body = document.querySelector('body');
+let changeColorId = null;
+
+startBtn.addEventListener('click', changeColorHandler);
+stopBtn.addEventListener('click', stopChangeColorHandler)
+
+function changeColorHandler() {
+  changeColorId = setInterval(() => {
+    const randomColor = randomIntegerFromInterval(0,colors.length); 
+    document.body.style.backgroundColor = colors[randomColor];
+  }, 1000);
+    startBtn.setAttribute('disabled', true);
+    stopBtn.removeAttribute('disabled');
+}
+
+function stopChangeColorHandler() {
+  clearInterval(changeColorId);
+  document.body.style.backgroundColor = colors[0];
+
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled', true);
+}
