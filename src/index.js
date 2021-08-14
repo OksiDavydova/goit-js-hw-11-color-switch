351bf5fc299c3b45a9e98625c233c0ab48b1c221
OksiDavydova <oksana.acva@gmail.com>
2021-08-14 15:47:12 +0300

    add function randomColor

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const randomIntegerFromInterval = (min, max) => {
 
 const startBtn = document.querySelector(['[data-action="start"]']); 
 const stopBtn = document.querySelector(['[data-action="stop"]']);
-const body = document.querySelector('body');
+const body = document.body;
 let changeColorId = null;
 
 startBtn.addEventListener('click', changeColorHandler);
@@ -22,17 +22,28 @@ stopBtn.addEventListener('click', stopChangeColorHandler)
 
 function changeColorHandler() {
   changeColorId = setInterval(() => {
-    const randomColor = randomIntegerFromInterval(0,colors.length); 
-    document.body.style.backgroundColor = colors[randomColor];
+    // const randomColor = randomIntegerFromInterval(0,colors.length-1); 
+    // body.style.backgroundColor = color[randomColor];
+    body.style.backgroundColor = randomColor();
   }, 1000);
-    startBtn.setAttribute('disabled', true);
-    stopBtn.removeAttribute('disabled');
+  startBtn.disabled = true;
+    // startBtn.setAttribute('disabled', true);
+    // stopBtn.removeAttribute('disabled');
 }
 
 function stopChangeColorHandler() {
   clearInterval(changeColorId);
-  document.body.style.backgroundColor = colors[0];
-
-  startBtn.removeAttribute('disabled');
-  stopBtn.setAttribute('disabled', true);
+  startBtn.disabled = false;
+  // document.body.style.backgroundColor = colors[0];
+  // startBtn.removeAttribute('disabled');
+  // stopBtn.setAttribute('disabled', true);
 }
+
+function randomColor() {
+  return `rgb(${randomIntegerFromInterval(0, 255)}, 
+  ${randomIntegerFromInterval(0, 255)}, ${randomIntegerFromInterval(0, 255)})`;
+}
+
+
+
+
